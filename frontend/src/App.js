c90b13650bf97ce9bfd98d592d6fb9c5c6e9bc4c
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-06-13 14:41:32 -0300

    Feat: introducing footer with basic style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Headers } from './components/containers/header/Headers';
+import { Footers } from './components/containers/footer/Footers';
 
 function App() {
   return (
     <div className="App">
       <Headers />
+      <Footers />
     </div>
   );
 }
