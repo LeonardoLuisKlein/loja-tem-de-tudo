b0fe4c36f76a6602536927e8071a56bffb0e5936
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-06-13 11:43:42 -0300

    Feat: installing saas and introducing header

---
+++ frontend/src/components/containers/header/Headers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Headers.scss";
+
+export const Headers = () => {
+  return (
+    <header>
+        <h1 id="headerTitle">Loja</h1>
+        <div id="linksContainer">
+            <a className="linkTitle">HOME</a> 
+            <a className="linkTitle">VENDAS</a>
+            <a className="linkTitle">CLIENTES</a> 
+            <a className="linkTitle">PRODUTOS</a>
+            <a className="linkTitle">CATEGORIAS</a> 
+            <a className="linkTitle">CIDADES</a>
+        </div>
+    </header>
+  )
+}
