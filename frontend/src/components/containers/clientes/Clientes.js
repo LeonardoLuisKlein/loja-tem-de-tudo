da6fe8fcd4bc78f25f48166668ca9dd9bb7cb442
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-06-23 19:09:32 -0300

    Feat: introducing new logics on products page with post, put and delete

--- frontend/src/components/containers/clientes/Clientes.js
+++ frontend/src/components/containers/clientes/Clientes.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { AiOutlinePlus, AiFillDelete, AiFillEdit } from 'react-icons/ai';
 import './Clientes.scss'
@@ -14,6 +14,22 @@ export const Clientes = () => {
   const [isValidName, setIsValidName] = useState(true);
   const [isValidEndereco, setIsValidEndereco] = useState(true);
   const [isValidCPF, setIsValidCPF] = useState(true);
+  const [editClientId, setEditClientId] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/v1/clientes');
+      const clientesData = response.data; // Dados retornados pelo backend
+      setClientes(clientesData); // Atualiza o estado global com os dados do backend
+      console.log(clientesData); // Exibe os dados no console ou realiza outras operações necessárias
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleNameChange = (value) => {
     setName(value);
@@ -50,113 +66,155 @@ export const Clientes = () => {
       const novoCliente = {
         nome,
         endereco,
-        CPF,
+        cpf: CPF,
         dataNasc
       };
 
       console.log('Dados a serem enviados:', novoCliente);
 
       try {
-        // Fazendo a solicitação POST ao backend
-        const response = await axios.post('http://localhost:8080/v1/clientes', novoCliente);
-        console.log(response.data); // Resposta do backend (opcional)
-        setClientes([...clientes, novoCliente]);
-        window.alert('Dados enviados com sucesso');
+        if (editClientId) {
+          // Atualizar cliente existente
+          await axios.put(`http://localhost:8080/v1/clientes/${editClientId}`, novoCliente);
+          const updatedClientes = clientes.map((cliente) => {
+            if (cliente.id_Cliente === editClientId) {
+              return { ...cliente, ...novoCliente };
+            }
+            return cliente;
+          });
+          setClientes(updatedClientes);
+          window.alert('Dados atualizados com sucesso');
+        } else {
+          // Criar novo cliente
+          const response = await axios.post('http://localhost:8080/v1/clientes', novoCliente);
+          console.log(response.data); // Resposta do backend (opcional)
+          setClientes([...clientes, response.data]);
+          window.alert('Dados enviados com sucesso');
+        }
       } catch (error) {
         console.error(error);
         // Lógica para lidar com o erro da solicitação
-        window.alert('Poblema');
+        window.alert('Erro ao enviar dados para o backend');
         console.log(error.response.data);
       }
     }
+
+    // Limpar os campos do formulário e remover o ID do cliente em edição
+    setName('');
+    setEndereco('');
+    setCPF('');
+    setDatana('');
+    setEditClientId(null);
   }
 
-  // const handleEdit = (productId) => {
-  //   // Lógica para lidar com a edição do produto com o ID fornecido
-  //   // Pode ser exibido um modal de edição, redirecionado para uma página de edição, etc.
-  //   console.log(`Editar produto com ID: ${productId}`);
-  // };
-  
-  // const handleDelete = (productId) => {
-  //   // Lógica para lidar com a exclusão do produto com o ID fornecido
-  //   // Remova o produto do estado "produtos"
-  //   const updatedProdutos = produtos.filter((produto) => produto.id !== productId);
-  //   setProdutos(updatedProdutos);
-  //   console.log(`Excluir produto com ID: ${productId}`);
-  // };
+  const handleDeleteCliente = async (clienteId) => {
+    try {
+      await axios.delete(`http://localhost:8080/v1/clientes/${clienteId}`);
+      const updatedClientes = clientes.filter((cliente) => cliente.id_Cliente !== clienteId);
+      setClientes(updatedClientes);
+      window.alert("Sucesso ao deletar");
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.log(error.response.data);
+      }
+      window.alert("Erro ao deletar");
+    }
+  };
+
+  const handleEditCliente = (clienteId) => {
+    const cliente = clientes.find((cliente) => cliente.id_Cliente === clienteId);
+    if (cliente) {
+      setName(cliente.nome);
+      setEndereco(cliente.endereco);
+      setCPF(cliente.cpf);
+      setDatana(cliente.dataNasc);
+      setEditClientId(clienteId);
+    }
+  };
 
   return (
     <div id="clientesForm">
       <h1>Clientes</h1>
       <div id="wrapFormCli">
-      <form id="formPrincipal">
-      <div id="inputsContainer">
-        <Inputs
-            labelText="Nome"
-            inputType="text"
-            inputId="bigInput"
-            placehInput="Nome"
-            errorMessage="Nome inválido"
-            containerType="bigContainer"
-            value={nome}
-            onChange={handleNameChange}
-            regex={/^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/}
-            isValid={isValidName}
-          />
-          <Inputs
-            labelText="Endereco"
-            inputType="text"
-            inputId="bigInput"
-            placehInput="Email"
-            errorMessage="Endereco inválido"
-            containerType="bigContainer"
-            value={endereco}
-            onChange={handleEnderecoChange} 
-            regex={/^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/}
-            isValid={isValidEndereco}
-          />
-          <Inputs
-            labelText="CPF"
-            inputType="text"
-            inputId="bigInput"
-            placehInput="CPF"
-            errorMessage="CPF inválido"
-            containerType="bigContainer"
-            value={CPF}
-            onChange={handleCPFChange}
-            regex={/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/}
-            isValid={isValidCPF}
-          />
-          <Inputs
-            labelText="Data Nasc"
-            inputType="date"
-            inputId="bigInput"
-            containerType="bigContainer"
-            value={dataNasc}
-            onChange={handleDataNaChange}
-          />
-        </div>
-        <button id="botaoCliente" onClick={handleFormSubmit}>Adicionar <AiOutlinePlus/></button>
-      </form>
-      <section id="tabelansky">
-        <div id="clienteTableUp">
-          <p>Nome</p>
-          <p>Endereco</p>
-          <p>CPF</p>
-          <p>Data Nasc</p>
-        </div>
-        <div id="clienteTableDown">
-          <p>Irineu</p>
-          <p>Rua ali</p>
-          <p>000.000.000-00</p>
-          <p>2000/06/20</p>
-          <div>
-          <button className="buttonED" /*onClick={() => handleEdit(produto.id)}*/><AiFillEdit /></button>
-          <button className="buttonED" /*onClick={() => handleDelete(produto.id)}*/><AiFillDelete /></button>
+        <form id="formPrincipal">
+          <div id="inputsContainer">
+            <Inputs
+              labelText="Nome"
+              inputType="text"
+              inputId="bigInput"
+              placehInput="Nome"
+              errorMessage="Nome inválido"
+              containerType="bigContainer"
+              value={nome}
+              onChange={handleNameChange}
+              regex={/^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/}
+              isValid={isValidName}
+            />
+            <Inputs
+              labelText="Endereco"
+              inputType="text"
+              inputId="bigInput"
+              placehInput="Endereco"
+              errorMessage="Endereco inválido"
+              containerType="bigContainer"
+              value={endereco}
+              onChange={handleEnderecoChange}
+              regex={/^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/}
+              isValid={isValidEndereco}
+            />
+            <Inputs
+              labelText="CPF"
+              inputType="text"
+              inputId="bigInput"
+              placehInput="CPF"
+              errorMessage="CPF inválido"
+              containerType="bigContainer"
+              value={CPF}
+              onChange={handleCPFChange}
+              regex={/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/}
+              isValid={isValidCPF}
+            />
+            <Inputs
+              labelText="Data Nasc"
+              inputType="date"
+              inputId="bigInput"
+              containerType="bigContainer"
+              value={dataNasc}
+              onChange={handleDataNaChange}
+            />
           </div>
-        </div>
-      </section>
+          <button id="botaoCliente" onClick={handleFormSubmit}>
+            {editClientId ? 'Editar' : 'Adicionar'} <AiOutlinePlus />
+          </button>
+        </form>
+        <table id="tabelaPrincipal">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Endereço</th>
+              <th>Data de Nascimento</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <tr key={cliente.id_Cliente}>
+                <td>{cliente.nome}</td>
+                <td>{cliente.endereco}</td>
+                <td>{cliente.dataNasc}</td>
+                <td>
+                  <button onClick={() => handleDeleteCliente(cliente.id_Cliente)}>
+                    <AiFillDelete />
+                  </button>
+                  <button onClick={() => handleEditCliente(cliente.id_Cliente)}>
+                    <AiFillEdit />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
-  )
-}
+  );
+};
